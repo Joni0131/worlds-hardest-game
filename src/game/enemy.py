2869862a56f6b96168cb9a12ea073badc6a96dd7
Joni0131 <Jonas.BergerFEU@web.de>
2022-04-06 23:24:04 +0200

    refactord the enemy class

--- src/game/enemy.py
+++ src/game/enemy.py
@@ -1,24 +1,59 @@
 import pygame
+import numpy as np
 
 class Enemy:
-    def __init__(self, x, y, width, height, vel=5, color=(0,0,255), right = True):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.vel = vel
-        self.color = color
-        self.right = right
+    def __init__(self, x, y, radius, speed, color):
+        self.pos = (x, y)  # position as tuple
+        self.radius = radius  # radius of the enemy
+        self.speed = speed  # speed in pixel
+        self.color = color  # main color
+        self.aimPoints = [self.pos]  # list of aims as [(x1, y1), (x2, y2)] ...
+        # the initial position is ofcourse a aim as well
+        self.velocitiesToPoint = [(0, 0)]  # list of speed vectors as [(velx1, vely1), (velx2, vely2)] ...
+        # the initial velocity is (0, 0) because it doesn't move
+        self.toleranceWindow = speed // 2  # this is the tolerance for reaching the aim
 
+    # draw the enemy at the current position
     def draw(self, screen):
-        pygame.draw.circle(screen, 0, (self.x, self.y), self.width+2, 2)
-        return pygame.draw.circle(screen, self.color, (self.x, self.y), self.width)
+        # first draw the line around the enemy in black
+        pygame.draw.circle(screen, 0, self.pos, self.radius, 1)
+        # second fill the inner with the enemy color
+        pygame.draw.circle(screen, self.color, self.pos, self.radius - 1)
+        return
 
+    # this function adds a new Point to where the object is moving in order
+    # calculate the needed velocities so calculation is done before runtime
+    def addMovementPoint(self, x, y):
+        # just append the next position
+        self.aimPoints.append((x, y))
+        # first velocity depends on last aim
+        # calculate the velocity by subtracting the last aim from the next one
+        # get the needed directions and then set the speed
+        velocity = tuple(np.sign(np.subtract((x, y), self.aimPoints[-2])) * self.speed)
+        # update last velocity
+        self.velocitiesToPoint[-1] = velocity
+        # calculate returning velocity the same way sign of aim - current
+        velocity = tuple(np.sign(np.subtract(self.aimPoints[0], (x, y))) * self.speed)
+        # append returning velocity
+        self.velocitiesToPoint.append(velocity)
 
-    def move(self, bound_x1, bound_x2):
-        if self.x < bound_x1 or self.x > bound_x2:
-            self.right = not self.right
-        if self.right:
-            self.x += self.vel
+    # create a move function that calculates the next position depending on the next aim and the velocity
+    def move(self):
+        # if there is no aimPoint it means the enemy is standing still so no update is happening
+        if len(self.aimPoints) == 1:
+            return
+        # if the enemy reached the aim position switch the aim positions and update the next velocity
+        if self.pos == self.aimPoints[0]:
+            self.aimPoints.append(self.aimPoints.pop(self.aimPoints.index(0)))
+            self.velocitiesToPoint.append(self.velocitiesToPoint.pop(self.velocitiesToPoint.index(0)))
+            # by returning we make sure it stops shortly at the aim
+            return
+        # now we calculate the next position by simply adding the velocity vector on top
+        newPos = np.add(self.pos, self.velocitiesToPoint[0])
+        # calculate the difference to the aim if it is smaller than speed/2 we are the closet possible to the aim
+        # so we set the new position to the aim
+        (diffX, diffY) = tuple(np.subtract(self.aimPoints[0], newPos))
+        if self.toleranceWindow * -1 < diffX <= self.toleranceWindow or self.toleranceWindow * -1 < diffY <= self.toleranceWindow:
+            self.pos = self.aimPoints[0]
         else:
-            self.x -= self.vel
+            self.pos = tuple(newPos)
