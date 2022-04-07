de1d1807a3f36dbb0cfed0b147c025fd2522673a
Joni0131 <Jonas.BergerFEU@web.de>
2022-04-07 17:30:58 +0200

    created rough sketch for the functionality of the editor

--- src/lvlEditor/editor.py
+++ src/lvlEditor/editor.py
@@ -5,3 +5,20 @@ class Editor:
 
     def __init__(self):
         return
+
+    # TODO all this
+    # plan for editor:
+    # first init a field of playable tiles width = 20 height = 10
+    # that means in reality there are 22*12 tiles
+    # set the others as out of bounds
+    # set certain keys for actions:
+        # remove tile from playing field on click
+        # set start
+        # set goal
+        # set start enemyPos all enemies are always starting and stopping in the middle of a tile
+        # set enemyAim points add transparent enemy draw, so it can be seen
+        # detect edges and add to corresponding tile
+            # draw edges afterwards
+        # save level
+            # ask for initial level name
+    # close editor
